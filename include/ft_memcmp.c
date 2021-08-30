f147e10136c54868e0bd601333ce99f6d0525ad8
pedro <pedromunizss@gmail.com>
2021-08-30 17:11:35 -0300

    remove int diff

--- include/ft_memcmp.c
+++ include/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 10:18:02 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/30 16:51:05 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/08/30 17:10:59 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int ft_memcmp (const void   *s1, const void *s2, size_t size)
 {
     unsigned char   *ptr_s1;
     unsigned char   *ptr_s2;
-    int diff;
+    
     ptr_s1 = (unsigned char *)s1;
     ptr_s2 = (unsigned char *)s2;
     while (*ptr_s1 == *ptr_s2 && size--)
