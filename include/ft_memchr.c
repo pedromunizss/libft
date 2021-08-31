735444b3d1836a69e398c85a284e95e5373f4770
pedro <pedromunizss@gmail.com>
2021-08-31 14:23:31 -0300

    reform function ft_memchr.c

--- include/ft_memchr.c
+++ include/ft_memchr.c
@@ -6,22 +6,22 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 17:15:41 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/30 17:26:49 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/08/31 14:20:55 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 'libft.h'
+#include "libft.h"
 
-void    *ft_memchr (const void  *s, int c, size_t   size)
+void	*ft_memchr(const void *s, int c, size_t size)
 {
-    unsigned char   *ptr_s;
-    unsigned char   *ptr_c;
+	unsigned char	*ptr_s;
 
-    ptr_s = (unsigned char *)s;
-    ptr_c = (unsigned char *)c;
-    while(*ptr_s != ptr_c || size--)
-        ptr_s++;
-    if (*ptr_s == ptr_c)
-        return (s);
-    return (NULL);
+	ptr_s = (unsigned char *)s;
+	while (size--)
+	{
+		if (*ptr_s == (unsigned char) c)
+			return (ptr_s);
+		ptr_s++;
+	}
+	return (NULL);
 }
