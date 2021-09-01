bb4dcdea1414e8e3c0feb9249666e2db3e4729e9
pedro <pedromunizss@gmail.com>
2021-08-31 22:04:24 -0300

    modify function ft_strlcpy

--- include/ft_strlcpy.c
+++ include/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 17:00:43 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/31 21:48:41 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/08/31 22:03:36 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ char	*ft_strlcpy(char *dest, const char *src, size_t size)
 	ptr_dest = (char *)dest;
 	ptr_src = (char *)src;
 	while (*ptr_src != '\0' && size--)
-	{
-		*ptr_dest = *ptr_src;
-		ptr_dest++;
-		ptr_src++;
-	}
+		*ptr_dest++ = *ptr_src++;
 	*ptr_dest = '\0';
 	return (dest);
 }
