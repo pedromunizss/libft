85d21022b0eda1e46597fe1915eb01e71f7ce5b1
pedro <pedromunizss@gmail.com>
2021-09-01 18:09:48 -0300

    modified ft_strlcpy and create ft_strlcat

--- include/ft_strlcat.c
+++ include/ft_strlcat.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/25 17:00:10 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/25 17:00:17 by pmuniz-s         ###   ########.fr       */
+/*   Created: 2021/08/31 22:06:02 by pmuniz-s          #+#    #+#             */
+/*   Updated: 2021/09/01 18:08:09 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char	*dest, char	*src);
+#include "libft.h"
 
-char	*ft_strcpy(char	*dest, char	*src)
-{	
-	int	i;
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	char	*ptr_src;
+	char	*ptr_dest;
+	size_t	len_dest;
+	size_t	len_src;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+	len_dest = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	ptr_dest = (char *)dest;
+	ptr_src = (char *)src;
+	ptr_dest += len_dest;
+	while (*ptr_src != '\0' && size--)
+		*ptr_dest++ = *ptr_src++;
+	*ptr_dest++ = '\0';
+	return (len_src + len_dest);
 }
--- include/ft_strlcpy.c
+++ include/ft_strlcpy.c
@@ -6,19 +6,21 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 17:00:43 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/31 22:03:36 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/01 17:26:28 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	*ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	char	*ptr_src;
 	char	*ptr_dest;
+	size_t	*len_src;
 
+	len_src = ft_strlen(src);
 	ptr_dest = (char *)dest;
 	ptr_src = (char *)src;
 	while (*ptr_src != '\0' && size--)
 		*ptr_dest++ = *ptr_src++;
 	*ptr_dest = '\0';
-	return (dest);
+	return (len_src);
 }
