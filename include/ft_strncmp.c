5363c0891c3e6143186cad2ba9e997ae4edd3b28
pedro <pedromunizss@gmail.com>
2021-09-02 14:25:35 -0300

    correção para n=0

--- include/ft_strncmp.c
+++ include/ft_strncmp.c
@@ -6,16 +6,18 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 12:11:01 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/02 12:36:17 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/02 14:24:26 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char	*s2, size_t n)
 {
-	unsigned char	*ptr_s1;
-	unsigned char	*ptr_s2;
+	if (n == 0)
+		return (0);
+	unsigned char *ptr_s1;
+	unsigned char *ptr_s2;
 
 	ptr_s1 = s1;
 	ptr_s2 = s2;
@@ -24,5 +26,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		ptr_s1++;
 		ptr_s2++;
 	}
-	return (*ptr_s1 - *ptr_s2);
+	return(*ptr_s1 - *ptr_s2);
 }
