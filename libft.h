4d5a24379d15791fc6a796daade8adc2251b510f
pedro <pedromunizss@gmail.com>
2021-09-06 20:20:57 -0300

    refresh header

--- libft.h
+++ libft.h
@@ -6,19 +6,28 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 10:12:00 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/08/31 21:50:21 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/06 20:19:01 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int	c);
-int ft_isalnum(int  c);
-int ft_isascii(int  c);
-int	ft_isprint(int	c);
-size_t	ft_strlen(const char    *s);
-void    *ft_memset (void    *b, int c, size_t   size);
-void    ft_bzero(void   *s, size_t  size);
-void    *ft_memcpy(void    *dest, const void   *src, size_t    size);
-void    *ft_memmove(void *dest, void *src, size_t size);
-char	*ft_strlcpy(char *dest, const char *src, size_t size);
+int		ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	*ft_memchr(const void *s, int c, size_t size);
+int		ft_memcmp(const void *s1, const void *s2, size_t size);
+void	*ft_memcpy(void *dest, const void *src, size_t size);
+void	*ft_memmove(void *dest, void *src, size_t size);
+void	*ft_memset(void *b, int c, size_t size);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	*ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
