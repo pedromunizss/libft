ee201960d7de1fb88dadb39a5ef4f68de1792f7f
pedro <pedromunizss@gmail.com>
2021-09-12 19:54:33 -0300

    added return from function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 15:35:47 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/11 11:15:33 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/12 12:36:24 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*ptr_set;
 	int		del;
 
-	ptr_set = *set;
-	swap = ft_strchr(ptr_s1, *ptr_set);
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	ptr_set = (char *)set;
+	ptr_s1 = (char *)s1;
 	while (*ptr_set != '\0')
 	{
-		while (ptr_s1 != NULL)
+		swap = ft_strchr(ptr_s1, *ptr_set);
+		while (swap != NULL)
 		{
-			del = ptr_s1 - ptr_s1;
+			del = swap - ptr_s1;
 			ft_memmove(ptr_s1 + del, ptr_s1 + del + 1, ft_strlen(ptr_s1) - 1);
 			swap = ft_strchr(ptr_s1, *ptr_set);
 		}
 		ptr_set++;
 	}
+	return (ptr_s1);
 }
