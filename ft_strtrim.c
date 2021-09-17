de4b7b9cdf48804430ba596df5303d4505a9ffd2
pedro <pedromunizss@gmail.com>
2021-09-17 15:40:55 -0300

    norminette

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 15:35:47 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/17 11:33:00 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/17 15:40:30 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int is_set_chr(const char c, const char *set)
+int	is_set_chr(const char c, const char *set)
 {
 	char	*ptr_set;
 
@@ -39,29 +39,29 @@ static int	count_set_chr(const char *s, const char *set)
 static int	count_rev_set_chr(const char *s, const char *set)
 {
 	int	i;
-	int j;
+	int	j;
 
 	i = ft_strlen(s) - 1;
 	j = 0;
 	while (is_set_chr(s[i], set))
-		{
-			i--;
-			j++;
-		}
+	{
+		i--;
+		j++;
+	}
 	return (j);
 }
 
 char	*ft_strtrim(const char *s, const char *set)
 {
-	int size;
-	int size_rev;
+	int		size;
+	int		size_rev;
 	char	*buff;
 	char	*ptr_buff;
-	int i;
+	int		i;
 
 	size = count_set_chr(s, set);
 	size_rev = count_rev_set_chr(s, set);
-	buff = (char*) malloc(sizeof(char) * ft_strlen(s) - size_rev - size + 1);
+	buff = (char *) malloc(sizeof (char) * ft_strlen(s) - size_rev - size + 1);
 	ptr_buff = buff;
 	i = size;
 	while (i < (int)ft_strlen(s) - size_rev)
