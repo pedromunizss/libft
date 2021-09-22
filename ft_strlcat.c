b56fa1dcab2b251388f9ff98f1f1a939f4b98a47
pedromunizss <pedromunizss@gmail.com>
2021-09-21 21:33:40 -0300

    chacking ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 22:06:02 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/21 15:27:19 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/21 21:24:03 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,15 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	char	*ptr_dest;
 	size_t	len_src;
 	size_t	len_dest;
-	size_t	len_cat;
 
 	ptr_src = (char *)src;
 	ptr_dest = (char *)dest;
 	len_src = ft_strlen(src);
 	len_dest = ft_strlen(dest);
-	len_cat = size - len_dest;
 	ptr_dest += len_dest;
 	if (size < len_dest)
 		return (size + len_src);
-	while (*ptr_src != '\0' && --len_cat)
+	while (*ptr_src && len_dest < size--)
 		*ptr_dest++ = *ptr_src++;
 	*ptr_dest++ = '\0';
 	return (len_src + len_dest);
---
+++ main/ft_strlcat.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <bsd/string.h>
+#include "/home/pedro/libft/libft.h"
+
+int main()
+{
+    char dest[30]; memset(dest, 0, 30);
+	char dest_original[30]; memset(dest_original, 0, 30);
+	char * src = (char *)"AAAAAAAAA";
+	dest[0] = 'B';
+
+    // teste 1
+	if(ft_strlcat(dest, src, 0) == strlen(src) && !strcmp(dest, "B"))
+        printf("sucess\n");
+    else
+		printf("fail\n");
+
+	// teste 2
+	dest[0] = 'B';
+	dest_original[0] = 'B';
+	size_t	len_ft;
+
+	len_ft = ft_strlcat(dest, src, 1);
+	if(ft_strlcat(dest, src, 1) == 10 && !strcmp(dest, "B"))
+	 	printf("sucess\n");
+	else
+		{
+			printf("fail\n");
+			printf("dest = %s\n", dest);
+			printf("ft_strlcat = %zu\n", len_ft);
+			printf("strlcat = %zu\n", strlcat(dest_original, src, 1));
+			printf("dest = %s\n", dest_original);
+		}
+
+	// teste 3
+	memset(dest, 'B', 4);
+	memset(dest_original, 'B', 4);
+	if(ft_strlcat(dest, src, 3) == 3 + strlen(src) && !strcmp(dest, "BBBB"))
+		printf("sucess\n");
+	else
+		printf("fail\n");
+	strlcat(dest_original, src, 3);
+	// teste 4
+	
+	if(ft_strlcat(dest, src, 6) == 13 && !strcmp(dest, "BBBBA"))
+		printf("sucess\n");
+	else
+	{
+		printf("fail\n");
+		printf("dest = %s\n", dest);
+		printf("ft_strlcat = %zu\n", len_ft);
+		printf("strlcat = %zu\n", strlcat(dest_original, src, 6));
+		printf("dest = %s\n", dest_original);
+	}
+	
+	// teste 5
+	memset(dest, 'C', 5);
+	
+}
