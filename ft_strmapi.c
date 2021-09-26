f99d062ace99652fe2296f874c6b56ad83f47e22
pedro <pedromunizss@gmail.com>
2021-09-26 16:29:23 -0300

    verified norminette

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 11:23:36 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/26 12:49:20 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/26 16:28:19 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	char	*ptr;
+	char			*ptr;
 	unsigned int	i;
 
 	ptr = ft_strdup(s);
 	i = 0;
-	while(ptr[i])
+	while (ptr[i])
 	{
 		ptr[i] = f(i, ptr[i]);
 		i++;
