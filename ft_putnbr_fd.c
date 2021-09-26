b594a9c997fc69ec66102e489a3213339529e4e7
pedromunizss <pedromunizss@gmail.com>
2021-09-26 15:54:03 -0300

    alterada condição quando nb for negativo

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pmuniz-s <pmuniz-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 19:09:15 by pmuniz-s          #+#    #+#             */
-/*   Updated: 2021/09/12 19:31:46 by pmuniz-s         ###   ########.fr       */
+/*   Updated: 2021/09/26 15:53:46 by pmuniz-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 void	ft_putnbr_fd(int nb, int fd)
 {
-	if (nb == -2147483648)
+	if (nb < 0)
 	{
-		ft_putnbr_fd(nb / 10, fd);
-		ft_putchar_fd('8', fd);
-	}
-	else if (nb < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(-1 * nb, fd);
+		if (nb == -2147483648)
+		{
+			ft_putnbr_fd(nb / 10, fd);
+			ft_putchar_fd('8', fd);
+		}
+		else
+		{
+			ft_putchar_fd('-', fd);
+			ft_putnbr_fd(-1 * nb, fd);
+		}
 	}
 	else
 	{
